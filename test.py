95e7d506c1985fcd5a5eaafdd0b7aeeb365dbeb4
KekPeng <daniellovesblue@gmail.com>
2018-12-04 14:48:33 +0800

    Test

---
+++ test.py
@@ -0,0 +1,8 @@
+number1=int(input('Enter the first number:'))
+number2=int(input('Enter the second number:'))
+sum=number1+number2
+diff=number1-number2
+product=number1*number2
+print('The sum of 2 numbers is:',sum)
+print('The diff of the 2 numbers is:',diff)
+print('THe product of the 2 numbers is',product)
