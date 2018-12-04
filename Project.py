3dd33922e4a6df310bd36f0a6499a6195b0cdfa2
KekPeng <daniellovesblue@gmail.com>
2018-12-04 15:19:44 +0800

    Initial Commit

---
+++ Project.py
@@ -0,0 +1,15 @@
+from flask import Flask , render_template
+app = Flask(__name__)
+
+@app.route("/")
+def Homepage():
+    return 'This is a homepage'
+
+@app.route("/profile/<name>")
+def profile(name):
+    return render_template("test.html", name=name)
+
+if __name__ =='__main__':
+    app.run(debug=True)
+
+
