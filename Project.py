6f01a6ccd4c1cb952a7a474d6c2b64ae4c64c746
Tan Kek Peng <daniellovesblue@gmail.com>
2018-12-09 11:32:46 +0800

    Commit1

--- Project.py
+++ Project.py
@@ -1,15 +1,22 @@
 from flask import Flask , render_template
 app = Flask(__name__)
 
+
 @app.route("/")
 def Homepage():
     return 'This is a homepage'
 
+
 @app.route("/profile/<name>")
 def profile(name):
-    return render_template("test.html", name=name)
+    return render_template("homepage.html", name=name)
 
-if __name__ =='__main__':
+@app.route("/map")
+def map():
+    return render_template("map.html")
+
+
+if __name__ == '__main__':
     app.run(debug=True)
 
 d
