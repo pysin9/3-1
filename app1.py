7b8aa2af96a4c15812b213e8fcbbd7ffe1aa959e
pysin9 <pysin9@gmail.com>
2018-12-11 15:28:38 +0800

    phyusin#idk

--- app1.py
+++ app1.py
@@ -1,17 +1,27 @@
-from flask import Flask, render_template, flash , url_for , redirect
+from flask import Flask, render_template, flash, url_for, redirect, session
 from form import *
 from user import *
 from flask import request
 
 
 app = Flask(__name__)
+SECRET_KEY = "yeah, not actually a secret"
+DEBUG = True
+app.config['SESSION_TYPE'] = 'filesystem'
 
 
 @app.route("/", methods=['GET', 'POST'])
 def login():
     login_form = LoginForm(request.form)
+    error = None
     if request.method == 'POST':
-        users = get_user(login_form.id.data, login_form.password.data)
+        user = get_user(login_form.id.data, login_form.password.data)
+        if user is None:
+            error = 'Wrong username and password'
+        else:
+            session['username'] = user.username
+            return redirect(url_for('index'))
+        flash(error)
     return render_template('login.html', form=login_form)
 
 
@@ -32,7 +42,12 @@ def register():
         flash(error)
     return render_template('signup.html', form=form)
 
-
+@app.route("/reset")
+def reset():
+    return render_template("reset.html")
 
 if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
     app.run(debug=True)
--- form.py
+++ form.py
@@ -1,18 +1,26 @@
 from wtforms import StringField, SubmitField, Form, PasswordField, IntegerField, BooleanField
 from wtforms import validators
+from wtforms.validators import email, DataRequired
 
 
 class LoginForm(Form):
-    id = StringField([validators.DataRequired('Please enter your name.')])
-    password = PasswordField([validators.DataRequired('Please enter your password.')])
+    id = StringField("Username", [validators.DataRequired('Please enter your name.')])
+    password = PasswordField("Password", [validators.DataRequired('Please enter your password.')])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
 
 class RegisterForm(Form):
-    id = StringField([validators.DataRequired('Please enter your name.')])
-    password = PasswordField([validators.DataRequired('Please enter your password.')])
-    town = StringField([validators.DataRequired("Please enter your location.")])
-    weight = IntegerField([validators.DataRequired("Please enter your weight in kilogram")])
-    height = IntegerField([validators.DataRequired("Please enter your height in meters")])
+    id = StringField("Username", [validators.DataRequired('Please enter your name.')])
+    password = PasswordField("Password", [validators.DataRequired('Please enter your password.')])
+    town = StringField("Town", [validators.DataRequired("Please enter your location.")])
+    weight = IntegerField("Weight", [validators.DataRequired("Please enter your weight in kilogram")])
+    height = IntegerField("Height", [validators.DataRequired("Please enter your height in meters")])
     submit = SubmitField('Register')
+
+
+class ResetForm(Form):
+    email = StringField("Email", validators=[DataRequired(), email()])
+    submit = SubmitField("Send Email")
+
+
