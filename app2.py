011cea01a7c8d9c8bc8383a3ca430f02a8303bf7
tiaaarr <tiapojaxd@gmail.com>
2018-12-11 13:52:30 +0800

    tia#2

---
+++ app2.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template,
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("Homepagee.html")
+
+
+@app.route('/ingredient')
+def ingredient():
+    return render_template('ingredients.html')
+
+if __name__ == '__main__':
+    app.run()
