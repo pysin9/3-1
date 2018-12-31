5a6ed7ff921462f058c8b8606a88b822b038d38a
marppel <marisadrake13@gmail.com>
2018-12-31 20:16:24 +0800

    fixed overwritten code

--- form.py
+++ form.py
@@ -1,7 +1,27 @@
-from wtforms import SubmitField, Form, IntegerField, StringField, PasswordField
+from wtforms import StringField, SubmitField, Form, PasswordField, IntegerField, BooleanField
 from wtforms import validators
-from wtforms.validators import NumberRange
+from wtforms.validators import email, DataRequired
+from markupsafe import Markup
 
+class LoginForm(Form):
+    id = StringField("Username", [validators.DataRequired('Please enter your name.')])
+    password = PasswordField("Password", [validators.DataRequired('Please enter your password.')])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+class RegisterForm(Form):
+    id = StringField("Username", [validators.DataRequired('Please enter your name.')])
+    password = PasswordField("Password", [validators.DataRequired('Please enter your password.')])
+    town = StringField("Town", [validators.DataRequired("Please enter your location.")])
+    weight = IntegerField("Weight", [validators.DataRequired("Please enter your weight in kilogram")])
+    height = IntegerField("Height", [validators.DataRequired("Please enter your height in meters")])
+    submit = SubmitField('Register')
+
+
+class ResetForm(Form):
+    email = StringField("Email", validators=[DataRequired(), email()])
+    submit = SubmitField("Send Email")
 
 class CalCount(Form):
     one = IntegerField('Calorie1', validators=[NumberRange(min=0)])
@@ -23,3 +43,4 @@ class Password(Form):
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match')])
     confirm = PasswordField('Re-enter Password')
+
