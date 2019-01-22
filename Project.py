aa59b721a6f7dc78562460ff078a7a4fc8215a72
pysin9 <pysin9@gmail.com>
2019-01-22 12:49:47 +0800

    Link to all pages

--- Project.py
+++ Project.py
@@ -11,14 +11,13 @@ import simplejson as json
 from flask.ext.cache import Cache
 
 app = Flask(__name__)
+
 UPLOAD_FOLDER = '/static/images/'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = "secret"
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-cache = Cache(app,config={'CACHE_TYPE': 'simple'})
-
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 app.secret_key = 'Secret Secret'
 
 @app.route('/')
@@ -39,11 +38,47 @@ def userprofile():
     return render_template('userprofile.html')
 
 
-@app.route("/map" ,methods=['GET','POST'])
+@app.route("/map" ,methods=['GET', 'POST'])
 @cache.cached(timeout=100)
 def map():
     data = MapPlace.query.all()
-    return render_template('map.html', data=data )
+    return render_template('map.html', data=data)
+
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    bmi = 0
+    form = UpdateProfile(request.form)
+    if request.method == 'POST':
+        bmi = float(request.form['weight'])/(float(request.form['weight'])*float(request.form['weight']))
+    return render_template('profile.html', bmi=bmi, form=form)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 
 @app.route("/login/", methods=['GET', 'POST'])
@@ -115,6 +150,7 @@ def reset():
         return render_template("reset.html")
     else:
         return render_template("reset.html")
+
 
 @app.route("/logout")
 def logout():
